5180a7a84acff773c66c65a3757386463605a21a
Sthons <534439357@qq.com>
2026-02-24 16:13:41 +0800

    feat(scripts): 添加示例脚本并更新.gitignore
    添加新的游戏脚本示例文件scripts/example.py，实现基本的游戏自动化功能。同时更新.gitignore文件，排除scripts/DNA目录和test/目录

---
+++ scripts/example.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import time
+import random
+from loguru import logger
+
+# 添加核心模块路径
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
+from core.base_game_script import BaseGameScript
+
+
+class ExampleScript(BaseGameScript):
+
+    def __init__(self, config_path=None):
+        # 调用父类初始化方法
+        super().__init__(config_path)
+
+        self.game_window_rect = {}  # 初始化游戏窗口信息
+        self.game_window_center_point = {}  # 初始化游戏窗口中心坐标
+
+    def on_start(self):
+        logger.info("游戏窗口定位中...")
+
+        # logger.info("目前所有窗口：")
+        # logger.info(self.window_locator.list_windows())
+
+        game_window = self.window_locator.get_window_rect(window_title="游戏窗口")
+
+        # if not game_window:
+        #     logger.error("未找到游戏窗口")
+        #     return
+
+        # 确保游戏窗口在前台
+        self.window_locator.bring_to_front(game_window["hwnd"])
+
+        logger.info("游戏窗口定位成功！")
+        self.game_window_rect = game_window
+        # 计算游戏窗口中心坐标
+        self.game_window_center_point = {
+            "left": game_window["left"] + game_window["width"] // 2,
+            "top": game_window["top"] + game_window["height"] // 2,
+        }
+
+        return
+
+    def game_logic(self):
+        """
+        游戏逻辑，子类必须实现此方法
+        """
+        # 鼠标移动
+        self.input_controller.move_mouse(
+            self.game_window_center_point["left"], self.game_window_center_point["top"]
+        )
+        # 鼠标中键点击
+        self.input_controller.click(button="middle")
+
+        # 模板图片识别
+        result = self.game_ops.appear(
+            "example\\example.png",  # 模板名称
+            timeout=5,  # 超时时间(秒)
+            threshold=0.6,  # 匹配阈值
+        )
+        if result.get("found", False):
+            # 键盘输入 r
+            self.input_controller.key_press("r")
+
+    def on_stop(self):
+        pass
+
+
+if __name__ == "__main__":
+
+    logger.info("启动自定义游戏脚本...")
+
+    # 创建脚本实例
+    script = ExampleScript()
+    # 启动脚本
+    script.start(1)
